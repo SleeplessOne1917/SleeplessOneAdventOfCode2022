fe45f4b63ad60a297007ab7b3a80deff195c05a7
SleeplessOne1917 <abias1122@gmail.com>
2022-12-01 07:41:06 -0500

    Initial commit

---
+++ 1/solution.ts
@@ -0,0 +1,51 @@
+import { open } from "fs/promises";
+import path from "path";
+
+export const solution1 = async () => {
+  console.log("Finding largest number of calories carried by a single elf.");
+  const input = await open(path.join("1", "input.txt"));
+
+  let highest = 0;
+  let current = 0;
+
+  for await (const line of input.readLines()) {
+    const lineNum = parseInt(line, 10);
+
+    if (isNaN(lineNum)) {
+      highest = current > highest ? current : highest;
+      current = 0;
+    } else {
+      current += lineNum;
+    }
+  }
+
+  console.log(`Largest number of calories: ${highest}`);
+};
+
+export const solution2 = async () => {
+  console.log(
+    "Finding sum of calories carries by the 3 highest calorie carraying elves"
+  );
+  const input = await open(path.join("1", "input.txt"));
+
+  const calorieTotals = [];
+  let current = 0;
+
+  for await (const line of input.readLines()) {
+    const lineNum = parseInt(line, 10);
+
+    if (isNaN(lineNum)) {
+      calorieTotals.push(current);
+      current = 0;
+    } else {
+      current += lineNum;
+    }
+  }
+
+  const total = [...calorieTotals]
+    .sort((a, b) => a - b)
+    .slice(-3)
+    .reduce((acc, cur) => acc + cur, 0);
+
+  console.log(`Largest number of calories: ${total}`);
+};
---
+++ index.ts
@@ -0,0 +1,70 @@
+import {
+  solution1 as day1Solution1,
+  solution2 as day1Solution2,
+} from "./1/solution";
+
+import { createInterface } from "readline/promises";
+
+const solutions = new Map([
+  [
+    1,
+    new Map<number, () => Promise<void>>([
+      [1, day1Solution1],
+      [2, day1Solution2],
+    ]),
+  ],
+]);
+
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const run = async () => {
+  let keepRunning = true;
+  do {
+    let day = await readline.question("Please select which challenge day  ");
+
+    let dayNum = parseInt(day, 10);
+
+    while (isNaN(dayNum) || !solutions.has(dayNum)) {
+      day = await readline.question("Invalid day. Please try again  ");
+      dayNum = parseInt(day, 10);
+    }
+
+    let challenge = await readline.question(
+      "Would you like to do challenge 1 or 2?  "
+    );
+
+    let challengeNum = parseInt(challenge, 10);
+
+    while (isNaN(challengeNum) || !solutions.get(dayNum)?.has(challengeNum)) {
+      challenge = await readline.question(
+        "Invalid challenge. Please try again.  "
+      );
+
+      challengeNum = parseInt(challenge, 10);
+    }
+
+    const solutionToRun = solutions.get(dayNum)?.get(challengeNum)!;
+    await solutionToRun();
+
+    let shouldContinue = await readline.question(
+      "Do you want to run another challenge?  "
+    );
+    while (
+      !(/^[yY].*/.test(shouldContinue) || /^[nN].*/.test(shouldContinue))
+    ) {
+      shouldContinue = await readline.question(
+        'Input must be "yes" or "no". Please try again  '
+      );
+    }
+
+    keepRunning = /^[yY].*/.test(shouldContinue);
+    console.log(keepRunning);
+  } while (keepRunning);
+
+  process.exit(0);
+};
+
+run();
